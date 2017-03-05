9c26ef9280bb5e879ac790c880655256187a361a
martin <msvonava@gmail.com>
2017-03-05 18:39:13 +0100

    comments

--- Main.py
+++ Main.py
@@ -3,30 +3,26 @@ import numpy as np
 
 img = cv2.imread('test_image.jpg', cv2.IMREAD_GRAYSCALE)
 
-# channels for color image
+# channels for color image, if needed, but I was told to use greyscale
 height, width = img.shape
 
 # blank image, will contain image and detected keypoints, just to show it works
-blank_image = np.zeros((height, width, 3), np.float32)
+# blank_image = np.zeros((height, width, 3), np.float32)
 
-
+# creating MSER keypoint detector
 mser = cv2.MSER_create()
 # detecting keypoints
 mser_areas = mser.detect(img)
 
-#creating sift to compute descriptors
+# creating SIFT object to compute descriptors
 sift = cv2.xfeatures2d.SIFT_create()
 
-# descriptors created from MSER keypoints teda aspon dufam
+# descriptors created from MSER keypoints (IMHO :D)
 descs = sift.compute(img, mser_areas)
 
+#  10 = Flag to specify the number of times the algorithm is executed using different initial labellings. The algorithm returns the labels that yield the best compactness. This compactness is returned as output. (?)
+# 1.0 = accuracy (TODO: Ask accuracy of WHAT?)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-#TypeError: data is not a numerical tuple -- v tomto pripade data = descs
-# AKO MAM SPRAVIT Z KEYPOINTOV TUPLE?
 ret,label,center = cv2.kmeans(data = descs[1], K = 6, bestLabels=None, criteria=criteria, attempts=10, flags=cv2.KMEANS_RANDOM_CENTERS)
 
-print("ret=",ret,"label=",label,"center=",center)
-
-#error: (-215) data0.dims <= 2 && type == CV_32F && K > 0 in function kmeans
-#ret,label,center = cv2.kmeans((100,200),6 ,None, criteria,10,cv2.KMEANS_RANDOM_CENTERS)
