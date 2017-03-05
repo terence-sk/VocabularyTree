2420395899723da6a917a91f7948c5de2ab980fe
martin <msvonava@gmail.com>
2017-03-05 18:25:45 +0100

    Initial Commit

--- Main.py
+++ Main.py
@@ -1,14 +1,13 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('test_image.jpg', cv2.IMREAD_GRAYSCALE);
+img = cv2.imread('test_image.jpg', cv2.IMREAD_GRAYSCALE)
 
 # channels for color image
 height, width = img.shape
 
 # blank image, will contain image and detected keypoints, just to show it works
-blank_image = np.zeros((height,width,3), np.float32)
-
+blank_image = np.zeros((height, width, 3), np.float32)
 
 
 mser = cv2.MSER_create()
@@ -25,9 +24,9 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
 #TypeError: data is not a numerical tuple -- v tomto pripade data = descs
 # AKO MAM SPRAVIT Z KEYPOINTOV TUPLE?
-#ret,label,center = cv2.kmeans(descs,6 ,None, criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+ret,label,center = cv2.kmeans(data = descs[1], K = 6, bestLabels=None, criteria=criteria, attempts=10, flags=cv2.KMEANS_RANDOM_CENTERS)
 
-
+print("ret=",ret,"label=",label,"center=",center)
 
 #error: (-215) data0.dims <= 2 && type == CV_32F && K > 0 in function kmeans
 #ret,label,center = cv2.kmeans((100,200),6 ,None, criteria,10,cv2.KMEANS_RANDOM_CENTERS)
